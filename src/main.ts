6df56ec3c2ddb78795dd460fa84a6b00f0f94db4
mario <mabg0630@gmail.com>
2023-10-27 22:38:30 -0500

    arreglo2

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,11 @@ async function bootstrap() {
   );
 
   // Configura las opciones de CORS
-  app.enableCors({
-    origin: 'http://localhost:3001', // Reemplaza con tus orígenes permitidos
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+app.enableCors({
+  origin: ['http://localhost:3000', 'http://localhost:3001'], // Reemplaza con tus orígenes permitidos
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
 
   await app.listen(process.env.PORT || 3000);
   logger.log(`Application is running on: ${process.env.PORT || 3000}`);
