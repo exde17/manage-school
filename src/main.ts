b24ea7962b62382f36f086747f74826b1b124561
unknown <mabg0630@gmail.com>
2024-03-25 21:20:57 -0500

    build de front2

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   // Configura las opciones de CORS
 app.enableCors({
-  origin: ['http://localhost:3000', 'http://localhost:3001','http://localhost:5173','http://exde17.com/','https://exde17.com/','https://render-school.onrender.com'], // Reemplaza con tus orígenes permitidos
+  origin: ['http://localhost:3000', 'http://localhost:3001','http://localhost:5173','http://exde17.com','https://exde17.com','https://render-school.onrender.com'], // Reemplaza con tus orígenes permitidos
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 });
