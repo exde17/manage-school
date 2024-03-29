30b3784418ef29040e80c103744246ec5ad03dec
unknown <mabg0630@gmail.com>
2024-03-29 11:08:20 -0500

    crud nivel-educativo

--- src/nivel_educativo/nivel_educativo.controller.ts
+++ src/nivel_educativo/nivel_educativo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { NivelEducativoService } from './nivel_educativo.service';
 import { CreateNivelEducativoDto } from './dto/create-nivel_educativo.dto';
 import { UpdateNivelEducativoDto } from './dto/update-nivel_educativo.dto';
@@ -8,27 +8,27 @@ export class NivelEducativoController {
   constructor(private readonly nivelEducativoService: NivelEducativoService) {}
 
   @Post()
-  create(@Body() createNivelEducativoDto: CreateNivelEducativoDto) {
+  async create(@Body() createNivelEducativoDto: CreateNivelEducativoDto) {
     return this.nivelEducativoService.create(createNivelEducativoDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.nivelEducativoService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.nivelEducativoService.findOne(+id);
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.nivelEducativoService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNivelEducativoDto: UpdateNivelEducativoDto) {
-    return this.nivelEducativoService.update(+id, updateNivelEducativoDto);
+  async update(@Param('id',ParseUUIDPipe) id: string, @Body() updateNivelEducativoDto: UpdateNivelEducativoDto) {
+    return this.nivelEducativoService.update(id, updateNivelEducativoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.nivelEducativoService.remove(+id);
+  async remove(@Param('id',ParseUUIDPipe) id: string) {
+    return this.nivelEducativoService.remove(id);
   }
 }
--- src/nivel_educativo/nivel_educativo.service.ts
+++ src/nivel_educativo/nivel_educativo.service.ts
@@ -1,26 +1,103 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNivelEducativoDto } from './dto/create-nivel_educativo.dto';
 import { UpdateNivelEducativoDto } from './dto/update-nivel_educativo.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NivelEducativo } from './entities/nivel_educativo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class NivelEducativoService {
-  create(createNivelEducativoDto: CreateNivelEducativoDto) {
-    return 'This action adds a new nivelEducativo';
+  constructor(
+    @InjectRepository(NivelEducativo)
+    private nivelEducativoRepository: Repository<NivelEducativo>
+  ) {}
+  async create(createNivelEducativoDto: CreateNivelEducativoDto) {
+    try {
+      const nivelEducativo = this.nivelEducativoRepository.create(createNivelEducativoDto);
+      await this.nivelEducativoRepository.save(nivelEducativo);
+
+      return {
+        message: 'Nivel educativo creado con exito',
+        data: nivelEducativo
+      }
+
+    } catch (error) {
+      return {
+        message: 'Error al crear nivel educativo',
+        data: error
+      }
+      
+    }
   }
 
-  findAll() {
-    return `This action returns all nivelEducativo`;
+  async findAll() {
+    try {
+      return await this.nivelEducativoRepository.find();
+    } catch (error) {
+      return {
+        message: 'Error al obtener niveles educativos',
+        data: error
+      }
+      
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} nivelEducativo`;
+  async findOne(id: string) {
+    try {
+      return await this.nivelEducativoRepository.findOne({
+        where: {
+          id
+        }
+      });
+    } catch (error) {
+      return {
+        message: 'Error al obtener nivel educativo',
+        data: error
+      }
+      
+    }
   }
 
-  update(id: number, updateNivelEducativoDto: UpdateNivelEducativoDto) {
-    return `This action updates a #${id} nivelEducativo`;
+  async update(id: string, updateNivelEducativoDto: UpdateNivelEducativoDto) {
+    try {
+      const nivelEducativo = await this.nivelEducativoRepository.findOne({
+        where: {
+          id
+        }
+      });
+
+      if (nivelEducativo) {
+        this.nivelEducativoRepository.merge(nivelEducativo, updateNivelEducativoDto);
+        return await this.nivelEducativoRepository.save(nivelEducativo);
+      }
+      else {
+        return {
+          message: 'Nivel educativo no encontrado',
+          data: null
+        }
+      }
+    } catch (error) {
+      return {
+        message: 'Error al actualizar nivel educativo',
+        data: error
+      }
+      
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} nivelEducativo`;
+  async remove(id: string) {
+    try {
+      await this.nivelEducativoRepository.delete(id);
+      return {
+        message: 'Nivel educativo eliminado con exito',
+        data: null
+      }
+    } catch (error) {
+      return {
+        message: 'Error al eliminar nivel educativo',
+        data: error
+      }
+      
+    }
   }
 }
